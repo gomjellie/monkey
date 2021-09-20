be2f6df1ee8f5b9eb7250fa38d3eb8ae00acdcd8
gomjellie <gomjellie@gmail.com>
2021-09-20 15:59:04 +0900

    chore: Lint tab -> 4 space

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,72 +3,72 @@ package lexer
 import "monkey/token"
 
 type Lexer struct {
-	input        string
-	position     int  // 입력에서 현재 위치
-	readPosition int  // 입력에서 현재 읽는 위치(현재 문자의 다음을 가리킴)
-	ch           byte // 현재 조사하고 있는 문자
+    input        string
+    position     int  // 입력에서 현재 위치
+    readPosition int  // 입력에서 현재 읽는 위치(현재 문자의 다음을 가리킴)
+    ch           byte // 현재 조사하고 있는 문자
 }
 
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
-	return l
+    l := &Lexer{input: input}
+    l.readChar()
+    return l
 }
 
 func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition += 1
+    if l.readPosition >= len(l.input) {
+        l.ch = 0
+    } else {
+        l.ch = l.input[l.readPosition]
+    }
+    l.position = l.readPosition
+    l.readPosition += 1
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
+    return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 func (l *Lexer) NextToken() token.Token {
-	var tok token.Token
+    var tok token.Token
 
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
-		tok = token.Token { Literal: "", Type: token.EOF}
-	default:
-		if isLetter(l.ch) {
-			tok.Literal = l.readIdentifier()
-			tok.Type = token.LookupIdent(tok.Literal)
-			return tok
-		} else {
-			tok = newToken(token.ILLEGAL, l.ch)
-		}
-	}
+    switch l.ch {
+    case '=':
+        tok = newToken(token.ASSIGN, l.ch)
+    case ';':
+        tok = newToken(token.SEMICOLON, l.ch)
+    case '(':
+        tok = newToken(token.LPAREN, l.ch)
+    case ')':
+        tok = newToken(token.RPAREN, l.ch)
+    case '{':
+        tok = newToken(token.LBRACE, l.ch)
+    case '}':
+        tok = newToken(token.RBRACE, l.ch)
+    case 0:
+        tok = token.Token { Literal: "", Type: token.EOF}
+    default:
+        if isLetter(l.ch) {
+            tok.Literal = l.readIdentifier()
+            tok.Type = token.LookupIdent(tok.Literal)
+            return tok
+        } else {
+            tok = newToken(token.ILLEGAL, l.ch)
+        }
+    }
 
-	l.readChar()
-	return tok
+    l.readChar()
+    return tok
 }
 
 func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.ch) {
-		l.readChar()
-	}
-	return l.input[position: l.position]
+    position := l.position
+    for isLetter(l.ch) {
+        l.readChar()
+    }
+    return l.input[position: l.position]
 }
 
 func isLetter(ch byte) bool {
-	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+    return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
