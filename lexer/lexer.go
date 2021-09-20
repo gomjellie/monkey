a002646c6ba4923156f086ae2f79b309003b6354
gomjellie <gomjellie@gmail.com>
2021-09-20 16:19:13 +0900

    feat: eat whitespace, handle comma, plus, digit

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,6 +32,7 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 func (l *Lexer) NextToken() token.Token {
     var tok token.Token
 
+    l.skipWhitespace()
     switch l.ch {
     case '=':
         tok = newToken(token.ASSIGN, l.ch)
@@ -45,12 +46,20 @@ func (l *Lexer) NextToken() token.Token {
         tok = newToken(token.LBRACE, l.ch)
     case '}':
         tok = newToken(token.RBRACE, l.ch)
+    case ',':
+        tok = newToken(token.COMMA, l.ch)
+    case '+':
+        tok = newToken(token.PLUS, l.ch)
     case 0:
         tok = token.Token { Literal: "", Type: token.EOF}
     default:
         if isLetter(l.ch) {
             tok.Literal = l.readIdentifier()
             tok.Type = token.LookupIdent(tok.Literal)
+            return tok
+        } else if isDigit(l.ch) {
+            tok.Type = token.INT
+            tok.Literal = l.readNumber()
             return tok
         } else {
             tok = newToken(token.ILLEGAL, l.ch)
@@ -71,4 +80,22 @@ func (l *Lexer) readIdentifier() string {
 
 func isLetter(ch byte) bool {
     return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+    for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+        l.readChar()
+    }
+}
+
+func (l *Lexer) readNumber() string {
+    position := l.position
+    for isDigit(l.ch) {
+        l.readChar()
+    }
+    return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+    return '0' <= ch && ch <= '9'
 }
